e6f94ca9e432b3d8d681461060e14577a34efe45
netstermonir <monirhossain6163@gmail.com>
2022-07-15 08:44:25 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'tw-elements';
 import App from './App';
 import './index.css';
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [require('tw-elements/dist/plugin')]
+  plugins: []
 };
